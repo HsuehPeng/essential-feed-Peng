a1a02efd4120f6a2d755b6f23364c915c049ac3a
Hsueh Peng Tseng <qaz5899971@gmail.com>
2023-06-24 17:39:59 +0800

    Create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Hsueh Peng Tseng on 2023/6/24.
+//
+
+import Foundation
+
+struct FeedItem {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let imageURL: URL
+}
