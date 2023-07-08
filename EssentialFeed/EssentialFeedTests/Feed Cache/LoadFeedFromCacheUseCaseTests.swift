8c005d3f5c53820bc0593eb382d39cac48d9a314
Hsueh Peng Tseng <qaz5899971@gmail.com>
2023-07-08 23:14:07 +0800

    Load command delivers no images on empty cache

--- EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -18,9 +18,9 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
 	
 	func test_load_requestsCacheRetrieval() {
 		let (sut, store) = makeSUT()
-
+		
 		sut.load() { _ in }
-
+		
 		XCTAssertEqual(store.receivedMessages, [.retrieve])
 	}
 	
@@ -28,7 +28,7 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
 		let (sut, store) = makeSUT()
 		let retrievalError = anyNSError()
 		let exp = expectation(description: "Wait for load completion")
-
+		
 		var receivedError: Error?
 		sut.load { result in
 			switch result {
@@ -39,11 +39,32 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
 			}
 			exp.fulfill()
 		}
-
+		
 		store.completeRetrieval(with: retrievalError)
 		wait(for: [exp], timeout: 1.0)
-
+		
 		XCTAssertEqual(receivedError as NSError?, retrievalError)
+	}
+	
+	func test_load_deliversNoImagesOnEmptyCache() {
+		let (sut, store) = makeSUT()
+		let exp = expectation(description: "Wait for load completion")
+		
+		var receivedImages: [FeedImage]?
+		sut.load { result in
+			switch result {
+			case let .success(images):
+				receivedImages = images
+			default:
+				XCTFail("Expected success, got \(result) instead")
+			}
+			exp.fulfill()
+		}
+		
+		store.completeRetrievalWithEmptyCache()
+		wait(for: [exp], timeout: 1.0)
+		
+		XCTAssertEqual(receivedImages, [])
 	}
 	
 	// MARK: - Helpers
