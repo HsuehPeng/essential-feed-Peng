202ad8e38caec4e4dab8b8bf39a3f1e0368a38d9
Hsueh Peng Tseng <qaz5899971@gmail.com>
2023-08-10 14:35:55 +0800

    Add willDisplay to preload image data since iOS 15+ change the cell lifecycle behavior

--- EssentialFeed/EssentialFeediOS/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/FeedViewController.swift
@@ -87,6 +87,10 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
 		cancelTask(forRowAt: indexPath)
 	}
 	
+	public override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+		tasks[indexPath] = imageLoader?.loadImageData(from: tableModel[indexPath.row].url) { _ in }
+	}
+	
 	public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
 		indexPaths.forEach { indexPath in
 			let cellModel = tableModel[indexPath.row]
