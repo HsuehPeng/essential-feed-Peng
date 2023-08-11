96e9c2a06f3dd15485eb99fd998df82845d63c29
Hsueh Peng Tseng <qaz5899971@gmail.com>
2023-08-11 14:17:38 +0800

    Move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Hsueh Peng Tseng on 2023/8/11.
+//
+
+struct FeedLoadingViewModel {
+	let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Hsueh Peng Tseng on 2023/8/11.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+	let feed: [FeedImage]
+}
