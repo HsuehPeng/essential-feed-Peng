31a4d770d376aacde38e18843d9687f45bc4841b
Hsueh Peng Tseng <qaz5899971@gmail.com>
2023-08-14 16:58:35 +0800

    Move ErrorView to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Hsueh Peng Tseng on 2023/8/14.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+	public var message: String?
+}
